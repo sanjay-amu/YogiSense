13c263b90a92430736bf9a0b6b66d29dbdf4df7b
sanjay-amu <160977037+sanjay-amu@users.noreply.github.com>
2025-03-01 16:40:35 -0500

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,1 +1,13 @@
+import PoseDetection from './PoseDetection/PoseDetection';
+
+function App() {
+  return (
+    <div>
+      <h1>YogiSense Pose Detection</h1>
+      <PoseDetection />
+    </div>
+  );
+}
+
+export default App;
 
